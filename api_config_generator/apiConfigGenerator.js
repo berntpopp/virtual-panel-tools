ee6c4972145b8e38fddc13f95f85bd04638b933f
Bernt Popp <bernt.popp@gmail.com>
2023-12-05 14:58:24 +0100

    feat: prepend essential API info in config files

--- api_config_generator/apiConfigGenerator.js
+++ api_config_generator/apiConfigGenerator.js
@@ -62,48 +62,64 @@ const isValidUrl = urlString => {
 /**
  * The main function that creates configuration files for each API endpoint.
  * Fetches the OpenAPI schema, processes each endpoint, and generates files in the specified format.
+ * It now includes essential API information such as host, schemes, basePath, securityDefinitions, 
+ * and security at the top of each configuration file for completeness and usability.
+ *
+ * @async
+ * @returns {Promise<void>} An empty promise indicating the completion of the operation.
  */
 const createConfigFiles = async () => {
-    if (!isValidUrl(argv.url)) {
-        console.error('Invalid URL provided.');
-        return;
-    }
+  if (!isValidUrl(argv.url)) {
+      console.error('Invalid URL provided.');
+      return;
+  }
 
-    try {
-        if (argv.verbose) console.log('Fetching the OpenAPI schema...');
-        const response = await axios.head(argv.url);
+  try {
+      if (argv.verbose) console.log('Fetching the OpenAPI schema...');
+      const schemaResponse = await axios.get(argv.url);
+      const { paths, host, schemes, basePath, securityDefinitions, security } = schemaResponse.data;
 
-        const schemaResponse = await axios.get(argv.url);
-        const paths = schemaResponse.data.paths;
+      const OUTPUT_DIR = path.join(__dirname, argv.output);
+      if (!fs.existsSync(OUTPUT_DIR)){
+          fs.mkdirSync(OUTPUT_DIR);
+      }
 
-        const OUTPUT_DIR = path.join(__dirname, argv.output);
-        if (!fs.existsSync(OUTPUT_DIR)){
-            fs.mkdirSync(OUTPUT_DIR);
-        }
+      if (argv.verbose) console.log('Processing endpoints...');
+      Object.keys(paths).forEach(endpoint => {
+          try {
+              const apiInfo = {
+                  ...(host && { host }), 
+                  ...(schemes && { schemes }), 
+                  ...(basePath && { basePath }), 
+                  ...(securityDefinitions && { securityDefinitions }), 
+                  ...(security && { security })
+              };
 
-        if (argv.verbose) console.log('Processing endpoints...');
-        Object.keys(paths).forEach(endpoint => {
-            try {
-                const config = { endpoint, ...paths[endpoint] };
-                const fileExtension = argv.format === 'yaml' ? 'yaml' : 'json';
-                const fileName = endpoint.replace(/\//g, '_').substring(1) + `.${fileExtension}`;
-                const filePath = path.join(OUTPUT_DIR, fileName);
+              const config = { 
+                  ...apiInfo,
+                  endpoint, 
+                  ...paths[endpoint]
+              };
 
-                const fileContent = argv.format === 'yaml' 
-                    ? yaml.dump(config) 
-                    : JSON.stringify(config, null, 2);
+              const fileExtension = argv.format === 'yaml' ? 'yaml' : 'json';
+              const fileName = endpoint.replace(/\//g, '_').substring(1) + `.${fileExtension}`;
+              const filePath = path.join(OUTPUT_DIR, fileName);
 
-                fs.writeFileSync(filePath, fileContent);
-                if (argv.verbose) console.log(`Generated ${fileExtension} config for: ${endpoint}`);
-            } catch (fileError) {
-                console.error(`Error writing file for endpoint ${endpoint}:`, fileError);
-            }
-        });
+              const fileContent = argv.format === 'yaml' 
+                  ? yaml.dump(config) 
+                  : JSON.stringify(config, null, 2);
 
-        console.log(`${package.name} v${package.version}: All configurations have been generated.`);
-    } catch (error) {
-        console.error('Error fetching or processing the OpenAPI schema:', error);
-    }
+              fs.writeFileSync(filePath, fileContent);
+              if (argv.verbose) console.log(`Generated ${fileExtension} config for: ${endpoint}`);
+          } catch (fileError) {
+              console.error(`Error writing file for endpoint ${endpoint}:`, fileError);
+          }
+      });
+
+      console.log(`${package.name} v${package.version}: All configurations have been generated.`);
+  } catch (error) {
+      console.error('Error fetching or processing the OpenAPI schema:', error);
+  }
 };
 
 createConfigFiles();
